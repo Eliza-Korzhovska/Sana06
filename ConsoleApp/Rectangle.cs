5e09f4b828bc28127b1ff0257c1f1e6bdb04bf5b
ElizaKorzhovska <ipz252_kev@student.ztu.edu.ua>
2026-01-14 14:05:43 +0200

    Створено похідний клас Rectangle

---
+++ ConsoleApp/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Rectangle : Shape
+    {
+        public double Length;
+        public double Width;
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+        public override double CalculateArea()
+        {
+            return Length * Width;
+        }
+        public override double CalculatePerimeter()
+        {
+            return 2 * (Length + Width);
+        }
+        public override void GetInfo()
+        {
+            Console.WriteLine($"Rectangle: Довжина = {Length}, Ширина = {Width}, Площа = {CalculateArea()}, Периметр = {CalculatePerimeter()}");
+        }
+    }
+}
