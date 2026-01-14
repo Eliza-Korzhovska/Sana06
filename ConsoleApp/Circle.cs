8741e4932d7a38d7a3113f4c3e6920c56d78a242
ElizaKorzhovska <ipz252_kev@student.ztu.edu.ua>
2026-01-14 13:59:32 +0200

    Створено похідний клас Circle

---
+++ ConsoleApp/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Circle : Shape
+    {
+        public double Radius;
+    }
+    public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+        public override void GetInfo()
+        {
+            Console.WriteLine($"Circle: Радіус = {Radius}, Площа = {CalculateArea()}, Периметр = {CalculatePerimeter()}");
+        }
+    }
+}
+    
